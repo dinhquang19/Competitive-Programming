e00812a83e850b0d71a203e7d8b63db58ea00f59
dinhquang19 <quyquang2006vx@gmail.com>
2026-07-08 09:09:02 +0700

    Add files via upload

---
+++ baitap.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define endl '\n'
+#define pb push_back
+#define int long long
+#define ii pair<int, int>
+#define MASK(i) (1LL << (i))
+#define sz(v) ((int)(v).size())
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define all(v) (v).begin(), (v).end()
+#define REP(i, n) for (int i = 0, _n = (n); i < _n; ++i)
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; ++i) 
+#define FORD(i, a, b) for (int i = (a), _b = (b); i >= _b; --i) 
+const int N = 1e6+5, M = 1e3+5, inf = 1e18, MOD = 1e9+7;//998244353;
+int n, m, x, y, res, cnt, sum, k;
+int a[N], b[N];
+
+void Solve() {
+    cin >> n;
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int _;cin >> _;while(_--)
+    Solve();
+}
